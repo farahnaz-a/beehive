d66995b9f7c8c1800800234c81f39245ee29bbfd
maksudur-rhmn <john.alim45@gmail.com>
2021-07-09 22:52:15 +0600

    filename

--- resources/views/customer/document.blade.php
+++ resources/views/customer/document.blade.php
@@ -35,8 +35,28 @@
                             document.getElementById('doc').click();
                       }); 
                   </script>
+                
                   <div class="paragraph">Taille Maximum: 10MB</div>
                 </div>
+                <div id="file-upload-filename"></div>
+                <script>
+                  var input = document.getElementById( 'doc' );
+                  var infoArea = document.getElementById( 'file-upload-filename' );
+
+                  input.addEventListener( 'change', showFileName );
+
+                  function showFileName( event ) {
+                    
+                    // the change event gives us the input it occurred in 
+                    var input = event.srcElement;
+                    
+                    // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
+                    var fileName = input.files[0].name;
+                    
+                    // use fileName however fits your app best, i.e. add it into a div
+                    infoArea.textContent = 'File name: ' + fileName + ' has been uploaded.';
+                  }
+                </script>
                 @error('doc')
                      <small style="color: red">{{ $message }}</small>
                 @enderror
